514e1f1882d6846b8d56c373f2d5cf11e1a51f0c
Mohamed Gamal <mohamedgamal3172@gmail.com>
2022-11-26 03:23:02 +0200

    sqlserverANDmysql

--- DatabaseProviders/MySqlDatabaseProvider.cs
+++ DatabaseProviders/MySqlDatabaseProvider.cs
@@ -6,56 +6,48 @@ using System.Threading.Tasks;
 
 namespace DoQL.DatabaseProviders
 {
-    public class SqliteDatabaseProvider : DatabaseProvider
+    public class MySqlDatabaseProvider:DatabaseProvider
     {
         public override List<string> GetDataTypes()
         {
-            List<string> types = new List<string> { "INT","INTEGER","TINYINT","SMALLINT","MEDIUMINT",
-                "BIGINT","UNSIGNED BIG INT","INT2","INT8","CHARACTER","VARCHAR","VARYING CHARACTER","NCHAR","NATIVE CHARACTER","NVARCHAR",
-                "TEXT","CLOB","BLOB","REAL","DOUBLE","DOUBLE PRECISION","FLOAT","NUMERIC","DECIMAL","BOOLEAN","DATE","DATETIME"};
+            List<string> types = new List<string> {"CHAR","VARCHAR","BINARY","VARBINARY","TINYBLOB","TINYTEXT","TEXT","BLOB","MEDIUMTEXT","MEDIUMBLOB","LONGTEXT","LONGBLOB","ENUM","SET","BIT","TINYINT",
+            "BOOL","BOOLEAN","SMALLINT","MEDIUMINT","INT","INTEGER","BIGINT","FLOAT","DOUBLE","DECIMAL","DEC","DATE","DATETIME","TIMESTAMP","TIME","YEAR"};
             return types;
         }
         public override bool ValidateTableName(string tableName)
         {
             List<string> keyWords = new List<string> { "ABORT", "ACTION", "ADD", "AFTER", "ALL", "ALTER", "ALWAYS", "ANALYZE", "AND", "AS", "ASC", "ATTACH", "AUTOINCREMENT", "BEFORE", "BEGIN", "BETWEEN", "BY", "CASCADE", "CASE", "CAST", "CHECK", "COLLATE", "COLUMN", "COMMIT", "CONFLICT",
                 "CONSTRAINT", "CREATE", "CROSS", "CURRENT", "CURRENT_DATE", "CURRENT_TIME", "CURRENT_TIMESTAMP", "DATABASE", "DEFAULT", "DEFERRABLE", "DEFERRED", "DELETE", "DESC", "DETACH", "DISTINCT", "DO", "DROP", "EACH", "ELSE", "END", "ESCAPE", "EXCEPT", "EXCLUDE", "EXCLUSIVE", "EXISTS", "EXPLAIN", "FAIL", "FILTER", "FIRST", "FOLLOWING", "FOR",
-                "FOREIGN", "FROM", "FULL", "GENERATED", "GLOB", "GROUP", "GROUPS", "HAVING", "IF", "IGNORE", "IMMEDIATE", "IN", "INDEX", "INDEXED", "INITIALLY", "INNER", "INSERT", "INSTEAD", "INTERSECT", "INTO", "IS", "ISNULL", "JOIN", "KEY", "LAST", "LEFT", "LIKE", "LIMIT", "MATCH",
+                "FOREIGN", "FROM", "FULL", "GENERATED", "COUNT", "GROUP", "GROUPS", "HAVING", "IF", "IGNORE", "IMMEDIATE", "IN", "INDEX", "INDEXED", "INITIALLY", "INNER", "INSERT", "INSTEAD", "INTERSECT", "INTO", "IS", "ISNULL", "JOIN", "KEY", "LAST", "LEFT", "LIKE", "LIMIT", "MATCH",
                 "MATERIALIZED", "NATURAL", "NO", "NOT", "NOTHING", "NOTNULL", "NULL", "NULLS", "OF", "OFFSET", "ON", "OR", "ORDER", "OTHERS", "OUTER", "OVER", "PARTITION", "PLAN", "PRAGMA", "PRECEDING", "PRIMARY", "QUERY", "RAISE", "RANGE", "RECURSIVE", "REFERENCES", "REGEXP", "REINDEX", "RELEASE",
                 "RENAME", "REPLACE", "RESTRICT", "RETURNING", "RIGHT", "ROLLBACK", "ROW", "ROWS", "SAVEPOINT", "SELECT", "SET", "TABLE", "TEMP", "TEMPORARY", "THEN", "TIES", "TO", "TRANSACTION", "TRIGGER", "UNBOUNDED", "UNION", "UNIQUE", "UPDATE", "USING", "VACUUM", "VALUES", "VIEW", "VIRTUAL", "WHEN", "WHERE", "WINDOW", "WITH", "WITHOUT" };
 
             List<char> chars = new List<char> { '#', '@', '$', '/', '&', '*', '+', '-', '!', '.' };
 
-            if(tableName.Contains(" ") == true)
+            string upperName = tableName.ToUpper();
+
+            if (upperName.Contains(" ") == true || upperName.Contains(".") == true || upperName.Contains("/") == true)
             {
                 return false;
             }
 
-            if (tableName.Length >= 7)
-            {
-                string name = tableName.Substring(0, 7);
-                if (name == "sqlite_")
-                {
-                    return false;
-                }
-            }
-
-            if (int.TryParse($"{tableName[0]}",out int result) == true)
+            if (int.TryParse($"{upperName[0]}", out int result) == true)
             {
                 return false;
             }
 
             foreach (char c in chars)
             {
-                if (tableName[0] == c)
+                if (upperName[0] == c)
                 {
                     return false;
-                    
+
                 }
             }
 
             foreach (string s in keyWords)
             {
-                if(tableName == s)
+                if (upperName == s)
                 {
                     return false;
                 }
--- DatabaseProviders/SqlServerDatabaseProvider.cs
+++ DatabaseProviders/SqlServerDatabaseProvider.cs
@@ -6,56 +6,48 @@ using System.Threading.Tasks;
 
 namespace DoQL.DatabaseProviders
 {
-    public class SqliteDatabaseProvider : DatabaseProvider
+    public class SqlServerDatabaseProvider : DatabaseProvider
     {
         public override List<string> GetDataTypes()
         {
-            List<string> types = new List<string> { "INT","INTEGER","TINYINT","SMALLINT","MEDIUMINT",
-                "BIGINT","UNSIGNED BIG INT","INT2","INT8","CHARACTER","VARCHAR","VARYING CHARACTER","NCHAR","NATIVE CHARACTER","NVARCHAR",
-                "TEXT","CLOB","BLOB","REAL","DOUBLE","DOUBLE PRECISION","FLOAT","NUMERIC","DECIMAL","BOOLEAN","DATE","DATETIME"};
+            List<string> types = new List<string> {"BIGINT","NUMERIC","BIT","SMALLINT","DECIMAL","SMALLMONEY","INT","TINYINT","MONEY","FLOAT","REAL","DATE","DATETIMEOFFSET","DATETIME2","SMALLDATETIME","DATETIME","TIME","CHAR","VARCHAR","TEXT","NCHAR"
+            ,"NVARCHAR","NTEXT","BINARY","VARBINARY","IMAGE","CURSOR","ROWVERSION","HIERARCHYID","UNIQUEIDENTIFIER","SQL_VARIANT","XML","SPATIALGEOMETRY","SPATIALGEOGRAPHY","TABLE"};
             return types;
         }
         public override bool ValidateTableName(string tableName)
         {
             List<string> keyWords = new List<string> { "ABORT", "ACTION", "ADD", "AFTER", "ALL", "ALTER", "ALWAYS", "ANALYZE", "AND", "AS", "ASC", "ATTACH", "AUTOINCREMENT", "BEFORE", "BEGIN", "BETWEEN", "BY", "CASCADE", "CASE", "CAST", "CHECK", "COLLATE", "COLUMN", "COMMIT", "CONFLICT",
                 "CONSTRAINT", "CREATE", "CROSS", "CURRENT", "CURRENT_DATE", "CURRENT_TIME", "CURRENT_TIMESTAMP", "DATABASE", "DEFAULT", "DEFERRABLE", "DEFERRED", "DELETE", "DESC", "DETACH", "DISTINCT", "DO", "DROP", "EACH", "ELSE", "END", "ESCAPE", "EXCEPT", "EXCLUDE", "EXCLUSIVE", "EXISTS", "EXPLAIN", "FAIL", "FILTER", "FIRST", "FOLLOWING", "FOR",
-                "FOREIGN", "FROM", "FULL", "GENERATED", "GLOB", "GROUP", "GROUPS", "HAVING", "IF", "IGNORE", "IMMEDIATE", "IN", "INDEX", "INDEXED", "INITIALLY", "INNER", "INSERT", "INSTEAD", "INTERSECT", "INTO", "IS", "ISNULL", "JOIN", "KEY", "LAST", "LEFT", "LIKE", "LIMIT", "MATCH",
+                "FOREIGN", "FROM", "FULL", "GENERATED", "COUNT", "GROUP", "GROUPS", "HAVING", "IF", "IGNORE", "IMMEDIATE", "IN", "INDEX", "INDEXED", "INITIALLY", "INNER", "INSERT", "INSTEAD", "INTERSECT", "INTO", "IS", "ISNULL", "JOIN", "KEY", "LAST", "LEFT", "LIKE", "LIMIT", "MATCH",
                 "MATERIALIZED", "NATURAL", "NO", "NOT", "NOTHING", "NOTNULL", "NULL", "NULLS", "OF", "OFFSET", "ON", "OR", "ORDER", "OTHERS", "OUTER", "OVER", "PARTITION", "PLAN", "PRAGMA", "PRECEDING", "PRIMARY", "QUERY", "RAISE", "RANGE", "RECURSIVE", "REFERENCES", "REGEXP", "REINDEX", "RELEASE",
                 "RENAME", "REPLACE", "RESTRICT", "RETURNING", "RIGHT", "ROLLBACK", "ROW", "ROWS", "SAVEPOINT", "SELECT", "SET", "TABLE", "TEMP", "TEMPORARY", "THEN", "TIES", "TO", "TRANSACTION", "TRIGGER", "UNBOUNDED", "UNION", "UNIQUE", "UPDATE", "USING", "VACUUM", "VALUES", "VIEW", "VIRTUAL", "WHEN", "WHERE", "WINDOW", "WITH", "WITHOUT" };
 
-            List<char> chars = new List<char> { '#', '@', '$', '/', '&', '*', '+', '-', '!', '.' };
+            List<char> chars = new List<char> { '#', '@', '$', '/', '&', '*', '+', '-', '!', '.', '_' };
 
-            if(tableName.Contains(" ") == true)
+            string upperName = tableName.ToUpper();
+
+            if (upperName.Contains(" ") == true || upperName.Contains(".") == true || upperName.Contains("/") == true)
             {
                 return false;
             }
 
-            if (tableName.Length >= 7)
-            {
-                string name = tableName.Substring(0, 7);
-                if (name == "sqlite_")
-                {
-                    return false;
-                }
-            }
-
-            if (int.TryParse($"{tableName[0]}",out int result) == true)
+            if (int.TryParse($"{upperName[0]}", out int result) == true)
             {
                 return false;
             }
 
             foreach (char c in chars)
             {
-                if (tableName[0] == c)
+                if (upperName[0] == c)
                 {
                     return false;
-                    
+
                 }
             }
 
             foreach (string s in keyWords)
             {
-                if(tableName == s)
+                if (upperName == s)
                 {
                     return false;
                 }
