980d43289224b1e94ccccbd09028bdd13a36cf0c
Ahmed kabbary <Ahmed kabbary@DESKTOP-BGP6EEP>
2022-12-05 14:10:53 +0200

    implement builder and factory

---
+++ DatabaseBuilder.cs
@@ -0,0 +1,47 @@
+﻿using DoQL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoQL
+{
+    class DatabaseBuilder
+    {
+        private string _name;
+        private DatabaseType _type;
+
+        public void SetName(string name)
+        {
+            this._name = name;
+        }
+
+        public void SetType(DatabaseType type)
+        {
+            this._type = type;
+        }
+
+        public void SetPassword(string password)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        public Database Build()
+        {
+            string id = Guid.NewGuid().ToString();
+            DateTime now = DateTime.Now;
+
+            return new Database
+            {
+                Id = id,
+                Name = _name,
+                Type = _type,
+                Created = now,
+                LastModified = now,
+                Tables = new List<Table>(),
+            };
+        }
+    }
+}
---
+++ DatabaseProvidersFactory.cs
@@ -0,0 +1,23 @@
+﻿using DoQL.DatabaseProviders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoQL
+{
+    class DatabaseProvidersFactory
+    {
+        public static DatabaseProvider GetDatabaseProvider(DatabaseType type)
+        {
+            return type switch
+            {
+                DatabaseType.MySQL => new MySqlDatabaseProvider(),
+                DatabaseType.SQLite => new SqliteDatabaseProvider(),
+                DatabaseType.SQLServer => new SqlServerDatabaseProvider(),
+                _ => throw new ArgumentException("Unknown database type"),
+            };
+        }
+    }
+}
