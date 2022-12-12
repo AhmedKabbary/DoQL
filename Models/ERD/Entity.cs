80433ff5e2b54c7521fa419274cddcd6217766de
Ahmed kabbary <Ahmed kabbary@DESKTOP-BGP6EEP>
2022-12-12 21:24:21 +0200

    IMovable and clean up

---
+++ Models/ERD/Entity.cs
@@ -0,0 +1,10 @@
+﻿namespace DoQL.Models.ERD
+{
+    public class Entity
+    {
+        public string EntityId { get; set; }
+        public string Name { get; set; }
+        public Point Position { get; set; }
+        public List<Attribute.Reference> Attributes { get; set; }
+    }
+}
