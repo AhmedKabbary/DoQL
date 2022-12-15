c3d996e8c1b31383e47004987673cd7eb7a377e0
Raneemmagdy11 <92212779+Raneemmagdy11@users.noreply.github.com>
2022-12-16 01:37:08 +0200

    Add files via upload

---
+++ DatabasesForm.cs
@@ -0,0 +1,38 @@
+﻿using DoQL.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoQL.Forms
+{
+    public partial class DatabasesForm : Form
+    {
+        public DatabasesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void DatabasesForm_Load(object sender, EventArgs e)
+        {
+            List<Database> list1 = new List<Database>();
+            flowLayoutPanel1.Controls.Clear();
+            foreach (Database i in list1)
+            {
+                DatabaseCard userControl = new DatabaseCard();
+                userControl.db = i;
+                flowLayoutPanel1.Controls.Add(userControl);
+            }
+        }
+    }
+}
