4c28b54f0a000930e4830dcfaaba75054975c12d
Abdullah Mahrous <rbkhg3@gmail.com>
2022-12-18 10:01:02 +0200

    validate duplicated table names

--- Utilities/DatabaseValidator.cs
+++ Utilities/DatabaseValidator.cs
@@ -1,5 +1,6 @@
 ﻿using DoQL.DatabaseProviders;
 using DoQL.Models;
+using DoQL.Models.ERD;
 
 namespace DoQL.Utilities
 {
@@ -8,11 +9,18 @@ namespace DoQL.Utilities
         public static void Validate(Database db)
         {
             DatabaseProvider databaseProvider = DatabaseProvidersFactory.GetDatabaseProvider(db.Type);
+
+            HashSet<string> errors = new HashSet<string>();
             foreach(var entity in db.Erd.Entities)
             {
                 if (!databaseProvider.ValidateTableName(entity.TableName))
                     throw new Exception($"Table name of {entity.DisplayName} entity is invalid");
+                
+                if(!errors.Add(entity.TableName))
+                    throw new Exception($"Table {entity.TableName} is duplicated ");
             }
+            errors.Clear();
+
             foreach (var attribute in db.Erd.Attributes)
             {
                 if (!databaseProvider.ValidateAttributeName(attribute.ColumnName))
@@ -20,11 +28,17 @@ namespace DoQL.Utilities
                 if (!databaseProvider.ValidateDataType(attribute.DataType))
                     throw new Exception($"Data type of {attribute.DisplayName} attribute is invalid");
             }
+            errors.Clear();
+
             foreach (var relationship in db.Erd.Relationships)
             {
-                if(relationship.IsManyToMany())
-                    if(!databaseProvider.ValidateTableName(relationship.TableName))
+                if (relationship.IsManyToMany())
+                {
+                    if (!databaseProvider.ValidateTableName(relationship.TableName))
                         throw new Exception($"Table name of {relationship.DisplayName} relationship is invalid");
+                    if (!errors.Add(relationship.TableName))
+                        throw new Exception($"Table {relationship.TableName} is duplicated ");
+                }
             }
         }
     }
