6ae2081ef16fad660bbdc5d74f047adce5945368
Ahmed kabbary <Ahmed kabbary@DESKTOP-BGP6EEP>
2022-12-19 02:14:24 +0200

    validate duplicated column names in the same entity or relationship

--- Utilities/DatabaseValidator.cs
+++ Utilities/DatabaseValidator.cs
@@ -10,33 +10,63 @@ namespace DoQL.Utilities
         {
             DatabaseProvider databaseProvider = DatabaseProvidersFactory.GetDatabaseProvider(db.Type);
 
-            HashSet<string> errors = new HashSet<string>();
-            foreach(var entity in db.Erd.Entities)
+            HashSet<string> entities = new HashSet<string>();
+            foreach (var entity in db.Erd.Entities)
             {
                 if (!databaseProvider.ValidateTableName(entity.TableName))
                     throw new Exception($"Table name of {entity.DisplayName} entity is invalid");
-                
-                if(!errors.Add(entity.TableName))
+
+                if (!entities.Add(entity.TableName))
                     throw new Exception($"Table {entity.TableName} is duplicated ");
             }
-            errors.Clear();
 
+            var entitiesAttributes = new Dictionary<string, HashSet<string>>(); // <entityId, attributesNames>
+            var relationshipsAttributes = new Dictionary<string, HashSet<string>>(); // <relationshipID, attributesNames>
             foreach (var attribute in db.Erd.Attributes)
             {
                 if (!databaseProvider.ValidateAttributeName(attribute.ColumnName))
                     throw new Exception($"Column name of {attribute.DisplayName} attribute is invalid");
                 if (!databaseProvider.ValidateDataType(attribute.DataType))
                     throw new Exception($"Data type of {attribute.DisplayName} attribute is invalid");
-            }
-            errors.Clear();
 
+                // check duplicated attributes in the same entity
+                Entity parentEntity = db.Erd.Entities.Find(e => e.AttributesReferences.Any(a => a.AttributeId == attribute.Id));
+                if (parentEntity != null)
+                {
+                    if (entitiesAttributes.ContainsKey(parentEntity.Id))
+                    {
+                        if (!entitiesAttributes[parentEntity.Id].Add(attribute.ColumnName))
+                        {
+                            throw new Exception($"{parentEntity.DisplayName} entity has duplicated column names");
+                        }
+                    }
+                    else entitiesAttributes[parentEntity.Id] = new HashSet<string>() { attribute.ColumnName };
+                }
+
+                // check duplicated attributes in the same relationship
+                Relationship parentRelationship = db.Erd.Relationships.Find(e => e.Attributes.Any(a => a.AttributeId == attribute.Id));
+                if (parentRelationship != null)
+                {
+                    if (relationshipsAttributes.ContainsKey(parentRelationship.Id))
+                    {
+                        if (!relationshipsAttributes[parentRelationship.Id].Add(attribute.ColumnName))
+                        {
+                            throw new Exception($"{parentRelationship.DisplayName} relationship has duplicated column names");
+                        }
+                    }
+                    else relationshipsAttributes[parentRelationship.Id] = new HashSet<string>() { attribute.ColumnName };
+                }
+            }
+
+            HashSet<string> relationships = new HashSet<string>();
             foreach (var relationship in db.Erd.Relationships)
             {
                 if (relationship.IsManyToMany())
                 {
                     if (!databaseProvider.ValidateTableName(relationship.TableName))
                         throw new Exception($"Table name of {relationship.DisplayName} relationship is invalid");
-                    if (!errors.Add(relationship.TableName))
+
+                    if (!relationships.Add(relationship.TableName))
                         throw new Exception($"Table {relationship.TableName} is duplicated ");
                 }
             }
